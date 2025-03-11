9279aa2cbe1faa0bc0dd57704fbec9d5cebd3031
unknown <242-000589@IMEPACAD.local>
2025-03-11 20:25:52 -0300

    branch-nicollas-sub

--- src/main/java/br/edu/imepac/Menu.java
+++ src/main/java/br/edu/imepac/Menu.java
@@ -6,8 +6,10 @@ public class Menu {
     public static void main(String[] args) {
         System.out.println("MENU ------ ");
         System.out.println("1 - SOMA");
+        System.out.println(("2 - SUB"));
 
         soma();
+        sub();
     }
 
     public static void soma() {
@@ -19,4 +21,17 @@ public class Menu {
 
         System.out.println("A soma dos números é: " + (num1 + num2));
     }
+
+    public static void sub() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite o primeiro número: ");
+        double num1 = scanner.nextDouble();
+        System.out.println("Digite o segundo número: ");
+        double num2 = scanner.nextDouble();
+
+        System.out.println("A subtração dos dois números é: " + (num1 - num2));
+
+    }
+
+
 }
