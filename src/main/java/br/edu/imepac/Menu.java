cbf031ce90ba0749c2c7e0a7c58671347e1c5098
JoaoViitoorr <joaovitoroliveirasimao9@gmail.com>
2025-03-11 20:20:46 -0300

    Commit da Multiplicação

--- src/main/java/br/edu/imepac/Menu.java
+++ src/main/java/br/edu/imepac/Menu.java
@@ -8,8 +8,9 @@ public class Menu {
         System.out.println("1 - SOMA");
 
         soma();
-    }
+        multiplicacao();
 
+    }
     public static void soma() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Digite o primeiro número: ");
@@ -18,5 +19,13 @@ public class Menu {
         int num2 = scanner.nextInt();
 
         System.out.println("A soma dos números é: " + (num1 + num2));
+    }
+    public static void multiplicacao(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite um numero: ");
+        int numero1 = scanner.nextInt();
+        System.out.println("Digite outro numero: ");
+        int numero2 = scanner.nextInt();
+        System.out.println("O resultado da multiplicação é " + numero1 * numero2 );
     }
 }
