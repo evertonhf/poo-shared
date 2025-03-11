576af43316654bca39e87b00a29276fa31fd94bd
alecioADS <alecio.programador@gmail.com>
2025-03-11 20:43:13 -0300

    Adicionado método de cálculo de porcentagem. - Alécio (alecioADS)

--- src/main/java/br/edu/imepac/Menu.java
+++ src/main/java/br/edu/imepac/Menu.java
@@ -6,8 +6,10 @@ public class Menu {
     public static void main(String[] args) {
         System.out.println("MENU ------ ");
         System.out.println("1 - SOMA");
+        System.out.println("2 - PORCENTAGEM");
 
         soma();
+        porcentagem();
     }
 
     public static void soma() {
@@ -18,5 +20,16 @@ public class Menu {
         int num2 = scanner.nextInt();
 
         System.out.println("A soma dos números é: " + (num1 + num2));
+    }
+
+    public static void porcentagem() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("DIGITE O VALOR TOTAL: ");
+        double total = scanner.nextDouble();
+        System.out.println("DIGITE A PORCENTAGEM (%): ");
+        double porcentagem = scanner.nextDouble();
+
+        double resultado = (total * porcentagem) / 100;
+        System.out.println("O RESULTADO DA PORCENTAGEM É: " + resultado);
     }
 }
