7bcaa38c1e517f7e2af8b4bf7595bc8d5cbac3e9
Vickytorie <Vitorgamer13br13@gmail.com>
2025-03-11 20:16:56 -0300

    Criei o método

--- src/main/java/br/edu/imepac/Menu.java
+++ src/main/java/br/edu/imepac/Menu.java
@@ -8,7 +8,18 @@ public class Menu {
         System.out.println("1 - SOMA");
 
         soma();
+        verificar();
     }
+
+    public static boolean verificar() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite o número e retorno se é par: ");
+        int num_a_verificar = scanner.nextInt();
+        return num_a_verificar % 2 == 0;
+
+    }
+
+
 
     public static void soma() {
         Scanner scanner = new Scanner(System.in);
