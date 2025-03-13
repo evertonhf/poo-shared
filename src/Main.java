a96cd9ab15096379689f9c949156041a18c69a71
unknown <242-000290@IMEPACAD.local>
2025-03-13 19:36:46 -0300

    primeiro prototipo

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+
+
+
+
+    }
+
+    public static void tabuada(int numero){
+        System.out.println("Aqui está a tabuada de " + numero + ":");
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(numero + " x " + i + " = " + (numero * i));
+        }
+    }
+
+}
