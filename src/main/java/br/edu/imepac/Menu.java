170e2f15b86cf183a83b5d3d7a68ec087a8359b1
mayconfidelis <mayconfidelis@gmail.com>
2025-03-13 19:46:39 -0300

    Método para calcular a média de notas inseridas.

--- src/main/java/br/edu/imepac/Menu.java
+++ src/main/java/br/edu/imepac/Menu.java
@@ -8,6 +8,7 @@ public class Menu {
         System.out.println("1 - SOMA");
 
         soma();
+
     }
 
     public static void soma() {
@@ -18,5 +19,24 @@ public class Menu {
         int num2 = scanner.nextInt();
 
         System.out.println("A soma dos números é: " + (num1 + num2));
+    }
+
+    public static class MediaNotas {
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+
+            System.out.println("Digite a primeira nota: ");
+            double nota1= scanner.nextDouble();
+
+            System.out.println("Digite a segunda nota: ");
+            double nota2= scanner.nextDouble();
+
+            System.out.println("Digite a terceira nota: ");
+            double nota3= scanner.nextDouble();
+
+            double media= (nota1+nota2+nota3)/3;
+            System.out.println("A média das notas é: " + media);
+
+        }
     }
 }
